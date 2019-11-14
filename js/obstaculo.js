fb31c61ae8bc5e81263c510609beef0173cb07c9
Leticia Berardo <leticiaberardo@gmail.com>
2019-11-14 15:09:15 -0300

    Cambios finales

--- js/obstaculo.js
+++ js/obstaculo.js
@@ -1,4 +1,6 @@
-/* Un objeto que representa a los obstaculos. Recibe un sprite que tendra la
+tilizan la herencia para objetos que tienen una
+correspondencia semántica entre sí pero un comportamiento
+específico diferente./* Un objeto que representa a los obstaculos. Recibe un sprite que tendra la
 imagen que lo representa y una potencia indicando cuando danio hace al chocar
 al jugador, ademas de los parametros comunes x, y, ancho y alto*/
 var Obstaculo = function (sprite, x, y, ancho, alto, potencia) {
